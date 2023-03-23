36e362a961b115a1d9cc254152e385f5d35f73eb
Nicolas Cavallin <nicavallin@gmail.com>
2023-03-23 17:52:49 +0100

    add sh4

--- aaa.sh
+++ aaa.sh
@@ -1,3 +1,3 @@
-NEXT_VERSION=$(echo yarn release minor --preRelease=rc --release-version)
+NEXT_VERSION_COMMAND=$(echo "yarn release minor --preRelease=rc --release-version")
 
 echo $(eval $NEXT_VERSION)
